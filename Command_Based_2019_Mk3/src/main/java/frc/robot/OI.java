ec9c84d1c6565ba3c1ed761b04e8fd09c5df402d
charlier999 <creepervenom10@gmail.com>
2019-02-19 19:51:40 -0800

    Elevator Hights 10

--- Command_Based_2019_Mk3/src/main/java/frc/robot/OI.java
+++ Command_Based_2019_Mk3/src/main/java/frc/robot/OI.java
@@ -10,7 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.buttons.*;
 import edu.wpi.first.wpilibj.Joystick;
 
-// Commmands
+// Commmands // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
 import frc.robot.commands.cmdBallIntake;
 import frc.robot.commands.cmdBallIntakeStop;
 import frc.robot.commands.cmdShift;
@@ -19,27 +19,29 @@ import frc.robot.commands.cmdClimberPull;
 import frc.robot.commands.cmdGrabberOC;
 import frc.robot.commands.cmdLinearActuator;
 import frc.robot.commands.cmdElevatorHeight;
-// import frc.robot.commands.cmdEncoderReset;
 
 public class OI {
-  public Boolean b_Grabber;
+  // Joysticks // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
 
-  // Joysticks
   public Joystick driver       = new Joystick(0);
   public Joystick operator     = new Joystick(1);
-  //public Joystick buttonPannel = new Joystick(2);
-  public Joystick testPannel   = new Joystick(2);
+  public Joystick buttonPannel = new Joystick(2);
 
-  // Driver Buttons
-  public Button gripperToggleClose      = new JoystickButton(driver, 3);
-  public Button gripperToggleOpen       = new JoystickButton(driver, 2);
+
+  // Driver Buttons -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
+
+  public Button gripperClose      = new JoystickButton(driver, 3);
+  public Button gripperOpen       = new JoystickButton(driver, 2);
+
   public Button outakeButton            = new JoystickButton(driver, 5);
   public Button intakeButton            = new JoystickButton(driver, 6); 
 
   public Button shifterUp               = new JoystickButton(driver, 7);
   public Button shifterDown             = new JoystickButton(driver, 8);
-  
-  // Operator Buttons
+   
+
+  // Operator Buttons // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
+
   public Button clawDropButton          = new JoystickButton(operator, 10);
   public Button clawUpButton            = new JoystickButton(operator, 9);
 
@@ -48,30 +50,20 @@ public class OI {
   public Button linearActuatorButtonIn  = new JoystickButton(operator, 3);
   public Button linearActuatorButtonOut = new JoystickButton(operator, 2);
 
-  // //Button Pannel Buttons
-  // public Button hatchLevel1Button       = new JoystickButton(buttonPannel, 1);
-  // public Button hatchLevel2Button       = new JoystickButton(buttonPannel, 2);
-  // public Button hatchLevel3Button       = new JoystickButton(buttonPannel, 3);
 
-  // public Button ballLevel1Button        = new JoystickButton(buttonPannel, 4);
-  // public Button ballLevel2Button        = new JoystickButton(buttonPannel, 5);
-  // public Button ballLevel3button        = new JoystickButton(buttonPannel, 6);
+  // Button Pannel Buttons // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
 
-  // public Button ballPickUpButton        = new JoystickButton(buttonPannel, 7);
+  public Button hatchLevel1Button       = new JoystickButton(buttonPannel, 1);
+  public Button hatchLevel2Button       = new JoystickButton(buttonPannel, 2);
+  public Button hatchLevel3Button       = new JoystickButton(buttonPannel, 3);
 
-  // public Button climberButton           = new JoystickButton(buttonPannel, 8);
+  public Button ballLevel1Button        = new JoystickButton(buttonPannel, 4);
+  public Button ballLevel2Button        = new JoystickButton(buttonPannel, 5);
+  public Button ballLevel3button        = new JoystickButton(buttonPannel, 6);
 
-  public Button hatchLevel1Button       = new JoystickButton(testPannel, 1); //A
-  public Button hatchLevel2Button       = new JoystickButton(testPannel, 2); //B
-  public Button hatchLevel3Button       = new JoystickButton(testPannel, 3); //X
+  public Button ballPickUpButton        = new JoystickButton(buttonPannel, 7);
 
-  public Button ballLevel1Button        = new JoystickButton(testPannel, 4); //Y
-  public Button ballLevel2Button        = new JoystickButton(testPannel, 5); //LB
-  public Button ballLevel3button        = new JoystickButton(testPannel, 6); //RB
-
-  public Button ballPickUpButton        = new JoystickButton(testPannel, 10); //Press right Joystick
-
-  public Button climberButton           = new JoystickButton(testPannel, 8);
+  public Button climberButton           = new JoystickButton(buttonPannel, 8);
 
 
   public Joystick getDriverJoystick()
@@ -86,6 +78,8 @@ public class OI {
 
   public OI()
   {
+    // Driver // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
+ 
     // Intake
     intakeButton.whileHeld(new cmdBallIntake(true, 1));
     intakeButton.whenReleased(new cmdBallIntakeStop());
@@ -97,52 +91,40 @@ public class OI {
     // Shifters
     shifterUp.whenPressed(new cmdShift(true));
     shifterDown.whenPressed(new cmdShift(false));
+       
+    // Grabber Open or Close
+    gripperClose.whenPressed(new cmdGrabberOC(true));
+    gripperOpen.whenPressed(new cmdGrabberOC(false));
 
+
+    // Operator // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
+   
     // Climbing Arms
-    clawDropButton.whenPressed(new cmdClawDrop(false));
-    clawUpButton.whenPressed(new cmdClawDrop(true));
-    climberPullButton.whileHeld(new cmdClimberPull());
+    clawDropButton.whenPressed(new cmdClawDrop(true));
+    clawUpButton.whenPressed(new cmdClawDrop(false));
+    climberPullButton.whileHeld(new cmdClimberPull(true));
 
-    // 
-    gripperToggleClose.whenPressed(new cmdGrabberOC(true));
-    gripperToggleOpen.whenPressed(new cmdGrabberOC(false));
-    // gripperToggle.whenPressed(new cmdGrabberOC());
-
+    // Linear Actuator
     linearActuatorButtonIn.whileHeld(new cmdLinearActuator(false));
     linearActuatorButtonOut.whileHeld(new cmdLinearActuator(true));
-    // encoderResetButton.whenReleased(new cmdEncoderReset());
 
+
+    // Button Pannel // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
+
+    // Hatch Levels
     hatchLevel1Button.whenPressed(new cmdElevatorHeight(354));
     hatchLevel2Button.whenPressed(new cmdElevatorHeight(186));
     hatchLevel3Button.whenPressed(new cmdElevatorHeight(18));
 
+    // Ball Levels
     ballLevel1Button.whenPressed(new cmdElevatorHeight(405));
     ballLevel2Button.whenPressed(new cmdElevatorHeight(237));
     ballLevel3button.whenPressed(new cmdElevatorHeight(69));
-
     ballPickUpButton.whenPressed(new cmdElevatorHeight(0));
 
-    //climberButton.whenPressed(new SomeOtherCmd());
+    // Auto Climb 
+    // climberButton.whenPressed(new SomeOtherCmd());
 
+// -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- 
   }
-
-
-//   //Drive Controller Axis's
-//   public double driverAxis0 = driver.getRawAxis(0);  // Left Thumb Stick  ~ X axis ~ +/- input
-//   public double driverAxis1 = driver.getRawAxis(1);  // Left Thumb Stick  ~ Y axis ~ +/- input
-//   public double driverAxis2 = driver.getRawAxis(2);  // Left Trigger               ~ only positive input
-//   public double driverAxis3 = driver.getRawAxis(3);  // Right Trigger              ~ only positive input
-//   public double driverAxis4 = driver.getRawAxis(4);  // Right Thumb Stick ~ X axis ~ +/- input
-//   public double driverAxis5 = driver.getRawAxis(5);  // Right Thumb Stick ~ Y axis ~ +/- input
-  
-//  driverAxis0 = driver.getRawAxis(0);
-//  driverAxis1 = driver.getRawAxis(1);
-
-  //Operator Controller Axis's
-  // public double operatorAxis0;  // Left Thumb Stick  ~ X axis ~ +/- input
-  // public double operatorAxis1;  // Left Thumb Stick  ~ Y axis ~ +/- input
-  // public double operatorAxis2;  // Left Trigger               ~ only positive input
-  // public double operatorAxis3;  // Right Trigger              ~ only positive input
-  // public double operatorAxis4;  // Right Thumb Stick ~ X axis ~ +/- input
-  // public double operatorAxis5;  // Right Thumb Stick ~ Y axis ~ +/- input
 }
--- Command_Based_2019_Mk3/src/main/java/frc/robot/subsystems/Elevator.java
+++ Command_Based_2019_Mk3/src/main/java/frc/robot/subsystems/Elevator.java
@@ -16,108 +16,117 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.Encoder;
 
-public class Elevator extends Subsystem {
-
-public Joystick operator     = new Joystick(1);
-WPI_TalonSRX rightElevatorMotor  = new WPI_TalonSRX(RobotMap.rightElevatorMotor);
-WPI_VictorSPX leftElevatorMotor   = new WPI_VictorSPX(RobotMap.leftElevatorMotor);
-
-public DoubleSolenoid p_elevatorBrake = new DoubleSolenoid(6, 7);
-
-// public DifferentialDrive elevatorBase;
-
-//public static int  elevatorDistance = new 
-public Encoder e_elevatorDrum = new Encoder(14, 15, false, Encoder.EncodingType.k4X);
-double elevatorDistance;
-double currentElevatorHight;
-
-
-
-public Elevator()
+public class Elevator extends Subsystem 
 {
-  rightElevatorMotor.setInverted(false);
-  leftElevatorMotor.setInverted(false);
-}
 
-public void elevatorBrake(Joystick joystick)
-{
-  if(joystick.getRawAxis(1) < .35 && joystick.getRawAxis(1) > -.35) 
+  public Joystick operator     = new Joystick(1);
+  WPI_TalonSRX rightElevatorMotor  = new WPI_TalonSRX(RobotMap.rightElevatorMotor);
+  WPI_VictorSPX leftElevatorMotor   = new WPI_VictorSPX(RobotMap.leftElevatorMotor);
+
+  public DoubleSolenoid p_elevatorBrake = new DoubleSolenoid(6, 7);
+
+  // public DifferentialDrive elevatorBase;
+
+  //public static int  elevatorDistance = new 
+  public Encoder e_elevatorDrum = new Encoder(14, 15, false, Encoder.EncodingType.k4X);
+  double elevatorDistance;
+  double currentElevatorHight;
+
+
+
+  public Elevator()
   {
-    p_elevatorBrake.set(Value.kForward);
-  }else{
-    p_elevatorBrake.set(Value.kReverse);
+    rightElevatorMotor.setInverted(false);
+    leftElevatorMotor.setInverted(false);
   }
-}
 
 
-public void autoElevator(boolean elevatorDirection, double elevatorSpeed) 
-{
-  elevatorSpeed = Math.abs(elevatorSpeed);
-  if(elevatorDirection)
-  {
-    rightElevatorMotor.set(elevatorSpeed);
-    leftElevatorMotor.set(elevatorSpeed);
-  }
-  else{
-    rightElevatorMotor.set(-elevatorSpeed);
-    leftElevatorMotor.set(-elevatorSpeed);
-   }
-}
-
-public void joystickElevator(Joystick joystick)
-{
-  rightElevatorMotor.set(joystick.getRawAxis(1));
-
-  leftElevatorMotor.set(joystick.getRawAxis(1));
-
-
-}
-
-public void testElevator(Joystick joystick)
-{
-  rightElevatorMotor.set(operator.getRawAxis(1));
-  leftElevatorMotor.set(operator.getRawAxis(5));
-}
-public void stop()
-{
-  rightElevatorMotor.set(0);
-  leftElevatorMotor.set(0);
-}
-
-public void encoderConsole() 
-{
+  // User Input //-=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=-  
   
-  elevatorDistance = e_elevatorDrum.getRaw();
-  System.out.println (elevatorDistance);
-}
+  public void elevatorBrake(Joystick joystick)
+  // applies the elevator brake based on the user input
+  {
+    if(joystick.getRawAxis(1) < .35 && joystick.getRawAxis(1) > -.35) 
+    // if the joystick axis is at 35 or gratter deggres at any angle,
+    //  the brake will be released 
+    {
+      p_elevatorBrake.set(Value.kForward); // released brake
+    }else{
+      p_elevatorBrake.set(Value.kReverse); // brake is applied
+    }
+  }
+  
+  public void joystickElevator(Joystick joystick)
+  // The elevator motors are spinned based on the user input
+  {
+    rightElevatorMotor.set(joystick.getRawAxis(1)); // 
+    leftElevatorMotor.set(joystick.getRawAxis(1));
+  }
 
-public void elevatorEncoderReset() 
-{
-  e_elevatorDrum.reset();
-}
 
-public void ElevatorHightset(double elevatorHight)
-{
-  currentElevatorHight = e_elevatorDrum.getDistance();
-  if(currentElevatorHight < elevatorHight)
+
+  // Automatic Input // -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=-  
+  
+
+
+  public void autoElevator(boolean elevatorDirection, double elevatorSpeed) 
   {
-    p_elevatorBrake.set(Value.kForward);
-    rightElevatorMotor.set(1);
-    leftElevatorMotor.set(1);
+    elevatorSpeed = Math.abs(elevatorSpeed);
+    if(elevatorDirection)
+    {
+      rightElevatorMotor.set(elevatorSpeed);
+      leftElevatorMotor.set(elevatorSpeed);
+    }else{
+      rightElevatorMotor.set(-elevatorSpeed);
+      leftElevatorMotor.set(-elevatorSpeed);
+    }
   }
-  if(currentElevatorHight > elevatorHight);
+
+  public void stop()
   {
-    p_elevatorBrake.set(Value.kForward);
-    rightElevatorMotor.set(-1);
-    leftElevatorMotor.set(-1);
+    rightElevatorMotor.set(0);
+   leftElevatorMotor.set(0);
   }
-  if(currentElevatorHight == elevatorHight)
+
+  public void encoderConsole() 
   {
-    p_elevatorBrake.set(Value.kReverse);
-    rightElevatorMotor.stopMotor();
-    leftElevatorMotor.stopMotor();
+   elevatorDistance = e_elevatorDrum.getRaw();
+    System.out.println (elevatorDistance);
   }
-}
+
+  public void elevatorEncoderReset() 
+  {
+    e_elevatorDrum.reset();
+  }
+
+  public void ElevatorHightset(double elevatorHight)
+  {
+    currentElevatorHight = e_elevatorDrum.getDistance();
+    if(currentElevatorHight < elevatorHight)
+    {
+      p_elevatorBrake.set(Value.kForward);
+      rightElevatorMotor.set(1);
+      leftElevatorMotor.set(1);
+    }
+
+    if(currentElevatorHight > elevatorHight);
+    {
+      p_elevatorBrake.set(Value.kForward);
+      rightElevatorMotor.set(-1);
+      leftElevatorMotor.set(-1);
+    }
+
+    if(currentElevatorHight == elevatorHight)
+    {
+      p_elevatorBrake.set(Value.kReverse);
+      rightElevatorMotor.stopMotor();
+      leftElevatorMotor.stopMotor();
+    }
+  }
+
+
+
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
